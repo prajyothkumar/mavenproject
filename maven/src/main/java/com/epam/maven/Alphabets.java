efb20b0430d36234143dbb6595b25abe7308bdec
janitha <janitha@LAPTOP-LR5OLQ2L>
2020-07-05 13:46:05 +0530

    first commit

---
+++ maven/src/main/java/com/epam/maven/Alphabets.java
@@ -0,0 +1,11 @@
+package com.epam.maven;
+
+public class Alphabets {
+	public static void main(String[] args) {
+        char c = '*';
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println(c + " is an alphabet.");
+        else
+            System.out.println(c + " is not an alphabet.");
+    }
+}
